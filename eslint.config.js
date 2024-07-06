339e2a2a9ec31d3c4557c24aa605cd17368a6761
mariaklyass <klyassmaria@gmail.com>
2024-07-06 18:43:28 +0300

    feat: add husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,59 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import eslintReact from "eslint-plugin-react";
-import eslintReactHooks from "eslint-plugin-react-hooks";
-import eslintReactRefresh from "eslint-plugin-react-refresh";
-import prettierPlugin from "eslint-plugin-prettier";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/build/**',
+      '**/dist/**',
+      '**/public/**',
+      '**/node_modules/**',
+      'vite-env.d.ts',
+      'eslint.config.js',
+      'package.json',
+      'tsconfig.json',
+      'vite.config.ts',
+    ],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2020,
+      },
+      // parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  // {
+  //   // enable jest rules on test files
+  //   files: ['test/**'],
+  //   ...jestPlugin.configs['flat/recommended'],
+  // },
+
+  eslintConfigPrettier
+);
